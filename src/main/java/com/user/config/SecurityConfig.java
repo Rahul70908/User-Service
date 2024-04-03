caf09ac3d86c5ef05cee14b757609474dc3a66b2
Rahul70908 <guptarahul7210@gmail.com>
2024-04-03 11:28:50 +0530

    Added Cors Config

--- src/main/java/com/user/config/SecurityConfig.java
+++ src/main/java/com/user/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -51,7 +56,18 @@ public class SecurityConfig {
                     c.addLogoutHandler(logoutHandler);
                     c.logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
                 })
+                .cors(cors -> corsConfigurationSource())
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
